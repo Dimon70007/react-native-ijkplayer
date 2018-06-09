7c38aed7cd4b70e8abc05af687e1d7c0747a1e31
Dmitry <dimon.fly.mail@mail.ru>
2018-06-09 03:52:06 +0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Downloading ijkplayer iOS framework (145M)"
-curl https:// somepathTo -> /IJKMediaFramework \
-  -o ios/IJKMediaFramework.framework/IJKMediaFramework \
-  -#
+# echo "Downloading ijkplayer iOS framework (145M)"
+# curl https:// somepathTo -> /IJKMediaFramework \
+#   -o ios/IJKMediaFramework.framework/IJKMediaFramework \
+#   -#
